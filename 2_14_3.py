6d3178d3d1a6cd041a22fed02c3e45562537c02c
Boris Stehnii <bstegniy@gmail.com>
2021-10-26 10:23:53 +0300

    Д/З

--- 2_14_3.py
+++ 2_14_3.py
@@ -16,14 +16,14 @@ class Fraction():
     def __truediv__(self, other):
         return self.number / other.number
 
-    def __float__(self):
-        return self.number
+    def __eq__(self, other):
+        return other == self.number
 
 
 x = Fraction(1/2)
 y = Fraction(1/4)
 
-assert x + y == float(Fraction(3/4)), "Error"
-assert x - y == float(Fraction(1/4)), "Error"
-assert x * y == float(Fraction(1/8)), "Error"
-assert x / y == float(Fraction(4/2)), "Error"
+assert x + y == Fraction(3/4), "Error"
+assert x - y == Fraction(1/4), "Error"
+assert x * y == Fraction(1/8), "Error"
+assert x / y == Fraction(4/2), "Error"
