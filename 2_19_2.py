69a658015e767c1b89101e43efadaa373c568e01
Boris Stehnii <bstegniy@gmail.com>
2021-11-09 16:23:14 +0200

    Д/З

--- 2_19_2.py
+++ 2_19_2.py
@@ -2,5 +2,7 @@ import sys
 
 
 print(sys.path)
+print(__file__)
+sys.path.append(fr"{__file__}")
 sys.path.append(r"C:\Users\Stehnii\PycharmProjects\Education_mod_2\exercise_2_19_1")
 print(sys.path)
