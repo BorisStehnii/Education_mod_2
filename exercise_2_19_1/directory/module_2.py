8c3cce83944e6c5ddde9b884b16c998140f01a47
Boris Stehnii <bstegniy@gmail.com>
2021-11-09 14:09:15 +0200

    Д/З

---
+++ exercise_2_19_1/directory/module_2.py
@@ -0,0 +1,6 @@
+def func_1(x, y):
+    return x**y
+
+
+def func_2(x, y):
+    return x+y
---
+++ exercise_2_19_1/main.py
@@ -0,0 +1,7 @@
+from directory.module_1 import func_2, func_1
+
+
+a = 5
+b = 6
+c = func_1(a, b)
+print(func_2(a, c))
